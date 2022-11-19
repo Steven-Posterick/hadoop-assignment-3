0ad7c356a97e8b9dd2fd0b7bc9ad726db42ef1eb
Steven <steven.posterick@outlook.com>
2022-11-19 00:54:11 -0600

    example

--- src/main/java/CommonNeighbor.java
+++ src/main/java/CommonNeighbor.java
@@ -3,19 +3,25 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-public class WordCount {
+public class CommonNeighbor {
   public static class TokenizerMapper
     extends Mapper<Object, Text, Text, Text>{
     private final Text valueText = new Text();
     private final Text keyText = new Text();
 
+    /*
+
+    111 112 139 171 346 523 524 561 613 830 925 984 1174 1260 1349 1424 1426 1529 1785 1994 2010 2060 2106 3989
+
+    (112,139) -> 111
+    (112,171) -> 111
+     */
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       StringTokenizer outerIterator = new StringTokenizer(value.toString());
 
@@ -28,12 +34,14 @@ public class WordCount {
 
         innerIterator.nextToken();
         for (int j = 1; innerIterator.hasMoreTokens(); j++) {
-          if (i == j || i > j) continue;
+          if (i == j) continue;
           StringBuilder sb = new StringBuilder();
           String innerToken = innerIterator.nextToken();
           sb.append(outerToken).append(",").append(innerToken);
 
           keyText.set(sb.toString());
+
+          // Write neighbors in a tuple (n1, n2) with the value of the linking neighbor
           context.write(keyText, valueText);
         }
       }
@@ -50,6 +58,7 @@ public class WordCount {
 
       StringBuilder sb = new StringBuilder();
 
+      // Append the
       while (iterator.hasNext()){
         sb.append(iterator.next());
         if (iterator.hasNext())
@@ -62,8 +71,8 @@ public class WordCount {
   }
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(WordCount.class);
+    Job job = Job.getInstance(conf, "common neighbor");
+    job.setJarByClass(CommonNeighbor.class);
     job.setMapperClass(TokenizerMapper.class);
     //job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(TextReducer.class);
